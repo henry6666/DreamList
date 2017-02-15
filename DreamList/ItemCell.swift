44d2f4d12f81baa2aeb1a7a2bfaf5dc58c560dab
Henry Aguinaga <aguinagahenry@gmail.com>
2017-02-15 10:28:11 +0100

    created tableView and implemented UITableViewDelegate and UITableViewDataSource in MainVC

---
+++ DreamList/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  DreamList
+//
+//  Created by Henry Aguinaga on 2017-02-15.
+//  Copyright © 2017 Henry Aguinaga. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+
+}
